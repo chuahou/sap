84439879ddaaee4dc87e9a6ec6578b11fddaf8ea
Chua Hou <human@chuahou.dev>
2019-08-25 19:18:22 +0800

    Add valued options tests for C

--- tests/src/ctests.c
+++ tests/src/ctests.c
@@ -1,6 +1,8 @@
+#include <assert.h>
+#include <malloc.h>
 #include <sap.h>
 
-int main()
+SapConfig setup_test_config()
 {
     SapConfig config =
     {
@@ -21,15 +23,15 @@ int main()
         .help = "Prints this help message"
     };
 
-    SapArgument posOpt =
+    SapArgument posOpt1 =
     {
-        .longopt = "POSITIONALARG",
+        .longopt = "POSITIONALARG1",
         .type = SAP_ARG_POSITIONAL,
         .required = 1,
         .help = "A positional argument"
     };
 
-    SapArgument valueOpt =
+    SapArgument valueOpt1 =
     {
         .shortopt = 'v',
         .longopt = "value",
@@ -38,12 +40,99 @@ int main()
         .help = "A valued option"
     };
 
-    SapArgument options[3] = { helpOpt, posOpt, valueOpt };
+    SapArgument flagOpt1 =
+    {
+        .shortopt = 'a',
+        .longopt = "aflag",
+        .type = SAP_ARG_OPTION,
+        .required = 0,
+        .help = "Flag A"
+    };
 
-    config.arguments = options;
-    config.argcount = 3;
+    SapArgument valueOpt2 =
+    {
+        .shortopt = 'c',
+        .longopt = "cvalue",
+        .type = SAP_ARG_OPTION_VALUE,
+        .required = 0,
+        .help = "Another valued option"
+    };
 
-    sap_print_help(config);
+    SapArgument flagOpt2 =
+    {
+        .shortopt = 'b',
+        .longopt = "bflag",
+        .type = SAP_ARG_OPTION,
+        .required = 0,
+        .help = "Flag B"
+    };
+
+    SapArgument posOpt2 =
+    {
+        .longopt = "ANOTHERPOSARG",
+        .type = SAP_ARG_POSITIONAL,
+        .required = 1,
+        .help = "Positional another time"
+    };
+
+    config.arguments = malloc(sizeof(SapArgument) * 7);
+
+    config.arguments[0] = helpOpt;
+    config.arguments[1] = posOpt1;
+    config.arguments[2] = valueOpt1;
+    config.arguments[3] = flagOpt1;
+    config.arguments[4] = valueOpt2;
+    config.arguments[5] = flagOpt2;
+    config.arguments[6] = posOpt2;
+    config.argcount = 7;
+
+    return config;
+}
+
+void test_value()
+{
+    SapConfig config = setup_test_config();
+
+    printf("Testing valued options...\n");
+
+    printf("Testing simple value\n");
+    const char *argv1[] = { "ctests", "-v", "value", "posarg" };
+    assert(sap_parse_args(config, 4, argv1) == 0);
+    assert(config.arguments[2].set == 1);
+    assert(strcmp(config.arguments[2].value, "value") == 0);
+
+    printf("Testing multiple values\n");
+    const char *argv2[] = { "ctests", "-v", "value", "-c", "cvalue", "posarg" };
+    assert(sap_parse_args(config, 6, argv2) == 0);
+    assert(config.arguments[2].set == 1);
+    assert(config.arguments[4].set == 1);
+    assert(strcmp(config.arguments[2].value, "value") == 0);
+    assert(strcmp(config.arguments[4].value, "cvalue") == 0);
+
+    printf("Testing incorrect multiple shortopt\n");
+    const char *argv3[] = { "ctests", "-vc", "value", "posarg" };
+    assert(sap_parse_args(config, 4, argv3) != 0);
+
+    printf("Testing value option followed by more options values\n");
+    const char *argv4[] = { "ctests", "-v", "--cvalue", "posarg" };
+    assert(sap_parse_args(config, 4, argv4) != 0);
+
+    printf("Testing longopt value\n");
+    const char *argv5[] = { "ctests", "--value", "value", "posarg" };
+    assert(sap_parse_args(config, 4, argv5) == 0);
+    assert(config.arguments[2].set == 1);
+    assert(strcmp(config.arguments[2].value, "value") == 0);
+
+    printf("Testing lack of required value\n");
+    const char *argv6[] = { "ctests", "posarg", "--cvalue", "abc" };
+    assert(sap_parse_args(config, 4, argv6) != 0);
+
+    printf("Value testing passed\n\n");
+}
+
+int main()
+{
+    test_value();
 
     return 0;
 }
