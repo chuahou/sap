2197f1011a0eaa8f16d9e8285b95e7ec2e036189
Chua Hou <human+github@chuahou.dev>
2020-04-28 11:47:36 +0800

    Fix too long comment in Introduction

--- include/sap.h
+++ include/sap.h
@@ -2,16 +2,16 @@
  * @mainpage
  * @section Introduction
  *
- * A simple argument parser written for C/C++ CLI applications. It is written in C
- * such that both only-C applications and C++ applications can make use of it.
+ * A simple argument parser written for C/C++ CLI applications. It is written in
+ * C such that both only-C applications and C++ applications can make use of it.
  *
- * It is inspired by some basic functionality from clap from Rust
- * in terms of the desired output, but is nowhere near as function-complete. I
- * only aim to properly implement in a reusable fashion the functionality I need
+ * It is inspired by some basic functionality from clap from Rust in terms of
+ * the desired output, but is nowhere near as function-complete. I only aim to
+ * properly implement in a reusable fashion the functionality I need
  * to use for my own projects.
  *
- * I chose not to use getopt or any other similar options as I would like my projects
- * to be portable across as many platforms as possible.
+ * I chose not to use getopt or any other similar options as I would like my
+ * projects to be portable across as many platforms as possible.
  */
 
  /**
